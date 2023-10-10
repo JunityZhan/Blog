d6c43036132402707b40413d055b2273cc1c73d8
LinkangZhan <linkangzhan@foxmail.com>
2023-10-10 01:39:32 -0400

    edit front end

---
+++ src/components/counter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(1)
+</script>
+
+<template>
+  <div>
+    <h1>Counter</h1>
+    <p>Count: {{ count }}</p>
+    <button @click="count++">Increment</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
